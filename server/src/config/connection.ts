90d9e166874bbd50783387f2b3230b2f83282d3a
095Logic <adipatel095@gmail.com>
2025-02-25 14:10:21 -0500

    Updated connection.ts to use a online database url

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,19 +3,15 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    decimalNumbers: true,
+  },
+});
 
 export default sequelize;
